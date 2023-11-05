a9465b5b7c41af2b683c352fdaef741f751c5fba
gracione <gracione2017@gmail.com>
2023-11-05 15:06:31 -0300

    socket io

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const Koa = require('koa');
+const http = require('http');
+const socket = require('socket.io');
+
+const app = new Koa();
+const server = http.createServer(app.callback());
+const io = socket(server);
+
+const SERVER_HOST = 'localhost';
+const SERVER_PORT = 8081;
+
+io.on('connection', socket => {
+    console.log('New connection');
+    socket.on('chat.message', data => {
+        console.log('message => ', data);
+        io.emit('chat.message', data);
+    })
+    socket.on('disconnect', () => {
+        console.log('Disconnect => A connection was disconnected');
+    });
+});
+
+server.listen(SERVER_PORT, () => {
+    console.log(`[HTTP] Listen => Server is running at http://${SERVER_HOST}:${SERVER_PORT}`)
+});
